139c615168fed92f2868371ee3cccc953a3769da
Pourixa <pouria.footballer@gmail.com>
2026-07-14 14:21:00 +0200

    Add 404

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ APP.use("/editCategory" , editCatRoute)
 APP.use("/item" ,editItemRoute )
 APP.use("/addItem",addItem)
 
+APP.use((req,res) => {
+    res.status(404).send("PAGE NOT FOUND!")
+})
 APP.listen(PORT,(err) => {
     if(err)
         throw err;
